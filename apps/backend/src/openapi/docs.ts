d89cf99ac2656c59f07ac2848b9b355fa491c741
brunonatanaelsr <202502594861@alunos.estacio.br>
2025-10-02 15:16:41 -0300

    fix(backend): align zod with openapi plugin

--- apps/backend/src/openapi/docs.ts
+++ apps/backend/src/openapi/docs.ts
@@ -1,17 +1,14 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
-import { OpenAPIGenerator } from '@asteasolutions/zod-to-openapi';
+import { OpenApiGeneratorV31 } from '@asteasolutions/zod-to-openapi';
 import { registry } from './routes';
 import { apiSpec } from './apiSpec';
 
 const router = express.Router();
 
 // Gera a documentação OpenAPI
-const openApiGenerator = new OpenAPIGenerator(registry.definitions, '3.1.0');
-const fullSpec = {
-  ...apiSpec,
-  ...openApiGenerator.generateDocument(apiSpec)
-};
+const openApiGenerator = new OpenApiGeneratorV31(registry.definitions);
+const fullSpec = openApiGenerator.generateDocument(apiSpec);
 
 // Serve a interface Swagger UI
 router.use('/', swaggerUi.serve);
